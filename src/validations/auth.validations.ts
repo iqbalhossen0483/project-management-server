8f3eaf7b222f2000e9ea0080b6a624042949df0d
Md Iqbal Hossen <iqbalhossen60483@gmail.com>
2026-01-21 19:31:57 +0600

    zod extra feild disabled

--- src/validations/auth.validations.ts
+++ src/validations/auth.validations.ts
@@ -1,26 +1,30 @@
 import { z } from 'zod';
 
-export const registerSchema = z.object({
-  name: z
-    .string('Name is required')
-    .min(3, 'Name must be at least 3 characters')
-    .max(30, 'Name must be at most 30 characters'),
+export const registerSchema = z
+  .object({
+    name: z
+      .string('Name is required')
+      .min(3, 'Name must be at least 3 characters')
+      .max(30, 'Name must be at most 30 characters'),
 
-  email: z.email('Invalid email address'),
+    email: z.email('Invalid email address'),
 
-  password: z
-    .string('Password is required')
-    .min(6, 'Password must be at least 6 characters')
-    .regex(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/,
-      'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character',
-    ),
-});
+    password: z
+      .string('Password is required')
+      .min(6, 'Password must be at least 6 characters')
+      .regex(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/,
+        'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character',
+      ),
+  })
+  .strict();
 
-export const loginSchema = z.object({
-  email: z.email('Invalid email address'),
-  password: z.string().min(6, 'Password must be at least 6 characters'),
-});
+export const loginSchema = z
+  .object({
+    email: z.email('Invalid email address'),
+    password: z.string().min(6, 'Password must be at least 6 characters'),
+  })
+  .strict();
 
 export type RegisterDto = z.infer<typeof registerSchema>;
 export type LoginDto = z.infer<typeof loginSchema>;
